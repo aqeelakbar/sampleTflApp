391b2da546be1b9f5e08d4b0961fa8ac8c52d461
Aqeel Akbar <aqeelakbar@Aqeels-MacBook-Pro.local>
2014-02-07 16:24:47 +0000

    changed the online/offline status and function of populating scope

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,25 @@
 
 angular.module('ngTflApp')
   .controller('MainCtrl', function ($scope, TFLStatus) {
-    TFLStatus.get(function (data, timeStamp, status){
-    	$scope.lineStatus = data;
-    	$scope.timeStamp = timeStamp;
-    	$scope.connection = status;
-      console.log(data)
-    });
+    
+  	var populateLines = function() {
+		TFLStatus.get(function (data, timeStamp, status){
+	    	// checks to see if the user is online or offline.
+	    	$scope.connection = TFLStatus.onLine();
+	    	$scope.lineStatus = data;
+	    	$scope.timeStamp = timeStamp;
+	    });
+	}
+
+
+	// refresh the lines to get the latest info
+    $scope.refreshLines= function() {
+		populateLines();
+	}
+
+	//load the lines
+	populateLines();
+
+	
+
   });
--- app/scripts/services/tflservice.js
+++ app/scripts/services/tflservice.js
@@ -13,14 +13,19 @@ angular.module('ngTflApp')
 			get: function(successCallback) {
 				$http.jsonp('http://www.schizdazzle.com/feed.php?callback=JSON_CALLBACK')
 				.success(function(data) {
-					successCallback(data.LineStatus, new Date(), 'Online');
+					successCallback(data.LineStatus, new Date());
 					localStorageService.set('ngTFLStatus', data.LineStatus);
 					localStorageService.set('ngTFLTimeStamp', new Date());
 				}).
 				error(function() {
 					console.log('ERROR: Could not get data.');
-					successCallback(localStorageService.get('ngTFLStatus'),  Date.parse(localStorageService.get('ngTFLTimeStamp')), 'Offline');
+					successCallback(localStorageService.get('ngTFLStatus'),  Date.parse(localStorageService.get('ngTFLTimeStamp')));
 				});
+			},
+
+			onLine: function() {
+				//checks to see the status of the user's connection
+				return navigator.onLine
 			}
 		};
 	});
